a2bc034927c728c598b85b20dec181ddcef4dca8
Noemy <145844934+nono969@users.noreply.github.com>
2026-07-20 20:54:56 -0300

    Add findByCod method to DisciplinaRepository

--- DisciplinaRepository.java
+++ DisciplinaRepository.java
@@ -1,9 +1,11 @@
 package br.edu.ifpb.escola.repository;
 
+import br.edu.ifpb.escola.model.entity.Disciplina;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import br.edu.ifpb.escola.model.entity.Disciplina;
+import java.util.Optional;
 
 @Repository
 public interface DisciplinaRepository extends JpaRepository<Disciplina, Long> {
+    Optional<Disciplina> findByCod(int cod);
 }
