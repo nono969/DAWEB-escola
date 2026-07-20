f8650857df54d38aad83b219458296144e70ad41
Noemy <145844934+nono969@users.noreply.github.com>
2026-07-20 20:54:22 -0300

    Refactor Disciplina class to use estudantes list

--- Disciplina.java
+++ Disciplina.java
@@ -1,25 +1,25 @@
 package br.edu.ifpb.escola.model.entity;
 
+import jakarta.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Transient;
 
 @Entity
 public class Disciplina {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    
+
+    @Column(unique = true, nullable = false)
+    private int cod;
+
     private String nome;
     private String professor;
     private String cargaHoraria;
 
-    @Transient
-    private List<Estudante> listaEstudantes = new ArrayList<>();
+    @OneToMany(mappedBy = "disciplinaMatriculada", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Estudante> estudantes = new ArrayList<>();
 
     public Disciplina() {}
 
@@ -29,20 +29,31 @@ public class Disciplina {
         this.cargaHoraria = cargaHoraria;
     }
 
+    // Getters e Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
+
+    public int getCod() { return cod; }
+    public void setCod(int cod) { this.cod = cod; }
+
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
+
     public String getProfessor() { return professor; }
     public void setProfessor(String professor) { this.professor = professor; }
+
     public String getCargaHoraria() { return cargaHoraria; }
     public void setCargaHoraria(String cargaHoraria) { this.cargaHoraria = cargaHoraria; }
-    public List<Estudante> getEstudantes() { return listaEstudantes; }
-    
+
+    public List<Estudante> getEstudantes() { return estudantes; }
+    public void setEstudantes(List<Estudante> estudantes) { this.estudantes = estudantes; }
+
     public void addEstudante(Estudante estudante) {
-        this.listaEstudantes.add(estudante);
+        this.estudantes.add(estudante);
         estudante.setD(this);
     }
-    
-    public int getQuantidadeEstudantes() { return listaEstudantes.size(); }
+
+    public int getQuantidadeEstudantes() {
+        return estudantes.size();
+    }
 }
