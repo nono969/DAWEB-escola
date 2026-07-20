618fae102ed03f9f7c2f283261e3a5c3e9f66e3f
Noemy <145844934+nono969@users.noreply.github.com>
2026-07-20 20:55:17 -0300

    Refactor EscolaApplication to use MenuPrincipalController

--- EscolaApplication.java
+++ EscolaApplication.java
@@ -1,16 +1,23 @@
 package br.edu.ifpb.escola;
+
+import br.edu.ifpb.escola.presentation.controller.MenuPrincipalController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EscolaApplication  implements CommandLineRunner{
+public class EscolaApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EscolaApplication.class, args);
-	}
-@Override
-	public void run (String... args) throws Exception {
-		System.out.println("Oi, esse é meu primeiro projeto.");
-	}
+    @Autowired
+    private MenuPrincipalController menuController;
+
+    public static void main(String[] args) {
+        SpringApplication.run(EscolaApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        menuController.iniciar();
+    }
 }
